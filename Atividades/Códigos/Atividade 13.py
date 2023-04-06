aa4081785efded98e6af99858b88de25f4bfdd85
Jetro Kepler <jetrokeplergomes@gmail.com>
2023-04-06 10:09:26 -0300

    Atividades.

---
+++ Atividades/Códigos/Atividade 13.py
@@ -0,0 +1,15 @@
+# Curso Técnico em Informática
+# Programação Orientada a Objetos
+# Exercício 13
+
+# Aluno: Jetro Kepler, 2º Informática.
+
+# 1 - Crie um programa em Python que pede um número inteiro ao usuário e calcule seu fatorial.
+
+class fatorial():
+
+    def __init__(self):
+
+        num = int(input("Informe um número: "))
+
+    def calculo_fatorial():
